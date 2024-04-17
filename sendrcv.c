705fe0b75e485f216a8896422b648959edb122c5
arh <arh@monkeypeppershoe.com>
2024-04-17 00:06:06 -0700

    addign another debug print

--- sendrcv.c
+++ sendrcv.c
@@ -17,8 +17,10 @@ int sendPDU(int clientSocket, uint8_t *dataBuffer, int lengthOfData){
     uint16_t pduLen = htons(lengthOfData);
     int numSent;
 
+    printf("Sending a PDU length of: %d", pduLen);
     /*add the length to the begginning of the pdu*/
     memcpy(sendPDU, &pduLen, sizeof(uint16_t));
+
 
     /*Copying the databuffer*/
     memcpy(sendPDU, dataBuffer + (sizeof(uint16_t)), sizeof(uint8_t) * lengthOfData);
@@ -36,16 +38,16 @@ int recvPDU(int socketNumber, uint8_t *dataBuffer, int bufferSize){
     uint16_t pduLen;
     int numRecv;
     /*receving the length first*/
-    if(recv(socketNumber, &pduLen, (sizeof(uint16_t)), MSG_WAITALL) < 0){
+    if(recv(socketNumber, &pduLen, 2, MSG_WAITALL) < 0){
         perror("recv error ");
     }
-    pduLen = htons(pduLen);
-    printf("pduLen= %d, BufferSize = %d\n",pduLen, htons(bufferSize));
+    pduLen = ntohs(pduLen);
+    printf("pduLen= %d, BufferSize = %d\n", ntohs(pduLen), ntohs(bufferSize));
     if(pduLen == 0){
         /*Connection was closed on the other side*/
         return 0;
-    }else if (pduLen > htons(bufferSize)) {
-        printf("Buffer too small to receive PDU (Missing %d bytes)", (pduLen - htons(bufferSize)));
+    }else if (pduLen > ntohs(bufferSize)) {
+        printf("Buffer too small to receive PDU (Missing %d bytes)", (ntohs(pduLen) - htons(bufferSize)));
         return 0;
     }
     
